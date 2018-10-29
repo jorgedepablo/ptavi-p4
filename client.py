3dcdbcf0653d132c21e1ce925fe7429e47e6bf16
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2018-10-29 10:23:03 +0100

    ejercio 5 solucionado lo de la entrada

--- client.py
+++ client.py
@@ -8,16 +8,20 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-LINE = sys.argv[3]
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = sys.argv[3:]
+except IndexError or ValueError:
+    sys.exit('Usage: python3 client.py "server" "port" "line" ')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
+    LINE = ' '.join(LINE)
+    print('Sending:', LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print('Received -- ', data.decode('utf-8'))
 
-print("Socket terminado.")
+print("Socket done.")
