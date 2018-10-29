b0c63e991e15cb071c8054996b002a53a027643e
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2018-10-29 13:54:11 +0100

    SERVER ej6 terminado

--- server.py
+++ server.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Clase (y programa principal) para un servidor de eco en UDP simple
+Class (and main program) for echo server in UDP simple
 """
 
 import socketserver
 import sys
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    dicc_Data = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b'Received request')
         for line in self.rfile:
-            print('The client send us ', line.decode('utf-8'))
+            DATA = line.decode('utf-8').split()
+            if DATA:
+                if DATA[0] == 'REGISTER':
+                    self.dicc_Data[DATA[1]] = self.client_address[0]
+                    self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+                else:
+                    self.wfile.write(b'Received request')
+                    print('The client send us: ', line.decode('utf-8'))
         print('IP: ', self.client_address[0], 'Port: ', self.client_address[1])
 
 if __name__ == "__main__":
-    # Listens at localhost ('')
+    # Listens at localhost ('') and setting port
     # and calls the EchoHandler class to manage the request
     PORT = int(sys.argv[2])
-    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print('Runnig echo server UDP...')
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
-        print('Server interrupt')
+        print('  Server interrupt')
