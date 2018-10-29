aa011f6ecae34db3d37d0d0d37e4ce272f7993d2
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2018-10-30 00:30:16 +0100

    intentando el 7 estoy cansado me voy a dormir

--- client.py
+++ client.py
@@ -11,13 +11,13 @@ import sys
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
-    LINE = sys.argv[3:]
+    USER = sys.argv[4]
+    EXPIRES = int(sys.argv[5])
 except IndexError or ValueError:
-    sys.exit('Usage: python3 client.py "server" "port" "line" ')
+    sys.exit('Usage: python3 client.py "server" "port" register "user-name"')
 
 def register():
-    USER = sys.argv[4]
-    REGISTER = 'REGISTER sip:'+ USER + ' SIP/2.0'
+    REGISTER = 'REGISTER sip:'+ USER + ' SIP/2.0\r\n' + 'Expires: ' + str(EXPIRES) 
     my_socket.send(bytes(REGISTER, 'utf-8') + b'\r\n\r\n')
 
 # Create the socket, config it and connect it to a server/port
@@ -25,11 +25,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     if sys.argv[3] == 'register':
         register()
-    else:
-        LINE = ' '.join(LINE)
-        print('Sending:', LINE)
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-
     data = my_socket.recv(1024)
     print('Received -- ', data.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -6,33 +6,42 @@ Class (and main program) for echo server in UDP simple
 
 import socketserver
 import sys
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    dicc_Data = {}
+    dicc_Users = {}
     def handle(self):
         """
         handle method of the server class
         (all requests will be handled by this method)
         """
+        received_mess = []
         for line in self.rfile:
-            DATA = line.decode('utf-8').split()
-            if DATA:
-                if DATA[0] == 'REGISTER':
-                    self.dicc_Data[DATA[1]] = self.client_address[0]
+            received_mess = line.decode('utf-8')
+            #received_mess.append(line.decode('utf-8'))
+            received_mess = ''.join(received_mess).split()
+
+            if received_mess:
+                #print(received_mess)
+                if received_mess[0] == 'REGISTER':
+                    print(received_mess)
+                    sip_address = received_mess[1].split(':')[1]
+                    expires_time = received_mess
+                    self.dicc_Users[sip_address] = self.client_address[0]
                     self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
-                else:
-                    self.wfile.write(b'Received request')
-                    print('The client send us: ', line.decode('utf-8'))
-        print('IP: ', self.client_address[0], 'Port: ', self.client_address[1])
+
 
 if __name__ == "__main__":
     # Listens at localhost ('') and setting port
     # and calls the EchoHandler class to manage the request
-    PORT = int(sys.argv[2])
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
+    try:
+        PORT = int(sys.argv[2])
+        serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
+    except IndexError or ValueError:
+        sys.exit('Usage: python3 server.py "port"')
 
     print('Runnig echo server UDP...')
     try:
